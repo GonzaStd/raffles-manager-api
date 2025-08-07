65a116a75f0226716f5c52bbe4d38253016edbd5
Gonzalo <gonzalo.vattino.secu@gmail.com>
2025-08-07 20:43:43 -0300

    Unused dependencies fixed

--- models/raffle.py
+++ models/raffle.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey, TIMESTAMP, CheckConstraint
+from sqlalchemy import Column, Integer, String, ForeignKey, TIMESTAMP
 from sqlalchemy.orm import relationship
 from database import Base
 
