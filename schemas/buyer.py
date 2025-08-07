8640fe7011ce56373179ba67db26e2ce44f7ce48
Gonzalo <gonzalo.vattino.secu@gmail.com>
2025-08-07 20:49:32 -0300

    Added "max_length"

--- schemas/buyer.py
+++ schemas/buyer.py
@@ -22,7 +22,7 @@ class BuyerUpdate(BaseModel):
     id: int
     name: Optional[str] = None
     phone: Optional[str] = Field(max_length=20, pattern=r'^\+?\s?\d[\d\s]{5,17}$')
-    email: Optional[EmailStr] = None
+    email: Optional[EmailStr] = Field(max_length=100)
 
     @model_validator(mode="after")
     def check_valid_fields(self):
