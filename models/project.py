a85c586035ce960078381b5fb63833c70725fc52
Gonzalo <gonzalo.vattino.secu@gmail.com>
2025-08-07 20:43:18 -0300

    Added model users and its relations

--- models/project.py
+++ models/project.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, Boolean, TIMESTAMP
+from sqlalchemy import Column, Integer, String, Text, Boolean, TIMESTAMP, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -6,9 +6,11 @@ from database import Base
 class Project(Base):
     __tablename__ = "projects"
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     name = Column(String(60), nullable=False, unique=True)
     description = Column(Text)
     creation_date = Column(TIMESTAMP, server_default="CURRENT_TIMESTAMP")
     is_active = Column(Boolean, default=True)
 
     raffle_sets = relationship("RaffleSet", back_populates="project")
+    user = relationship("Users", back_populates="projects")
---
+++ models/users.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text, Boolean, TIMESTAMP
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(100), nullable=False, unique=True)
+    password_hash = Column(Text, nullable=False)
+    projects = relationship("Project", back_populates="user")
