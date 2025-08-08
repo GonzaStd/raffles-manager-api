c2b26571d60a4d57a8ec1e6b19c64b877fc10e32
Gonzalo <gonzalo.vattino.secu@gmail.com>
2025-08-07 21:06:56 -0300

    Added Users

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@ from .project import Project
 from .raffle import Raffle
 from .raffleset import RaffleSet
 from .buyer import Buyer
+from .users import Users
 
-__all__ = ["Project", "RaffleSet", "Raffle", "Buyer"]
+__all__ = ["Project", "RaffleSet", "Raffle", "Buyer", "Users"]
