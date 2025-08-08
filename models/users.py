88375ac1c36c01e3d6e393a0d90c464076017721
Gonzalo <gonzalo.vattino.secu@gmail.com>
2025-08-07 22:27:47 -0300

    Update model User to match structure.sql

--- models/users.py
+++ models/users.py
@@ -1,12 +1,16 @@
-from sqlalchemy import Column, Integer, String, Text
-from sqlalchemy.orm import relationship
-from database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean, DateTime
+from database.connection import Base
+from datetime import datetime, timezone
 
 
-class Users(Base):
-    __tablename__ = "users"
+class User(Base):
+    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(100), nullable=False, unique=True)
-    password_hash = Column(Text, nullable=False)
-    projects = relationship("Project", back_populates="user")
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(32), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(128), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now(timezone.utc), nullable=False)
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
