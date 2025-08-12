bacb437c74ff693b0d36affd6fb326ea0379dbb1
Gonzalo <gonzalo.vattino.secu@gmail.com>
2025-08-12 09:12:36 -0300

    Dynamic configuration of port

--- main.py
+++ main.py
@@ -34,9 +34,11 @@ app.include_router(raffle.router, tags=["Raffles"])
 
 if __name__ == "__main__":
     import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
         reload=True
     )
